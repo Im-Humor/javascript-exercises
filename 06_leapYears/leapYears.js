ff6e0890e89caa09724e888e557bebdc4229809f
Im-Humor <orangite@gmail.com>
2023-04-24 16:14:28 -0700

    added logic for leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if (yearInput % 4 != 0) {
+        return false;
+    }
+    else if (yearInput % 100 == 0 && yearInput % 400 != 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
 };
 
 // Do not edit below this line
