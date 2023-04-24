a8ba4610266b092c73a44e238e85a156170149b2
Im-Humor <orangite@gmail.com>
2023-04-24 13:45:32 -0700

    Add logic for reversing strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strInput) {
+    let strArray = strInput.split("");
+    let arrOut = [];
+    for (let i of strArray) {
+        arrOut.unshift(i);
+    }
+    return arrOut.join("");
 };
 
 // Do not edit below this line
