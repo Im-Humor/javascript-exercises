e93811a7b09cf5ae295800ef7a9b93dbcc4c3b0f
Im-Humor <orangite@gmail.com>
2023-05-26 13:53:20 -0700

    add plaindrome check function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str1) {
+    let forArr = [];
+    let backArr = [];
+    for (x = 0; x < str1.length; x++) {
+        if (str1[x].toLowerCase() != str1[x].toUpperCase()) {
+            forArr.push(str1[x].toLowerCase());
+        }
+    }
+    for (x = forArr.length-1; x >= 0; x--) {
+        backArr.push(forArr[x]);
+    }
+    for (x = 0; x < forArr.length; x++) {
+        if (forArr[x] != backArr[x]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
