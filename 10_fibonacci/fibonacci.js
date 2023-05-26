5a7d040454107ae733193ec3d01737a1e6b725e6
Im-Humor <orangite@gmail.com>
2023-05-26 14:07:53 -0700

    add fibonacci check function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(input1) {
+    if (typeof parseInt(input1) != 'number' || parseInt(input1) < 0) {
+        return "OOPS";
+    }
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for (x = 0; x < input1; x++) {
+        a = b;
+        b = c;
+        c = a + b;
+    }
+    return c;
 };
 
 // Do not edit below this line
