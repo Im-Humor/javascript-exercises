a3172b09b04b33d184c4d4a04d2cd15cf4d44478
Im-Humor <orangite@gmail.com>
2023-05-26 14:15:33 -0700

    create title finding function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array1) {
+    array2 = [];
+    for (x = 0; x < array1.length; x++) {
+        array2.push(array1[x].title);
+    }
+    return array2;
 };
 
 // Do not edit below this line
