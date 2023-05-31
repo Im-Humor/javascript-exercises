c781f9b18b1c3de6e09e1d9cc7dbde540bcefb89
Im-Humor <orangite@gmail.com>
2023-05-31 16:25:36 -0700

    complete function for tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    let ageComp = 0;
+// first we iterate through the list of people
+// to find the oldest person's age and set equal to ageComp
+    for(x = 0; x < people.length; x++) {
+        let age = 0;
+        if (people[x].hasOwnProperty("yearOfDeath") == false) {
+            age = currentYear - people[x].yearOfBirth;
+        }
+        else {
+            age = (people[x].yearOfDeath - people[x].yearOfBirth);
+        }
+        if (age > ageComp) {
+            ageComp = age;
+        }
+    }
+    // then we find the specific user based on the oldest age
+    // making sure to account for deathyear not provided
+    return people.find(person => {
+        let age = 0;
+        if (person.hasOwnProperty("yearOfDeath") == false) {
+            age = currentYear - person.yearOfBirth;
+            return (age == ageComp);
+        }
+        else {
+            age = (person.yearOfDeath - person.yearOfBirth);
+            return (age == ageComp);
+        }
+    });
 };
 
 // Do not edit below this line
